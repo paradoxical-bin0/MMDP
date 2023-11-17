7251372d615791c2caf64ec6039f1246a1234d77
Shreyanshi Bhatt <shreyadbhatt154@gmail.com>
2023-11-17 19:14:45 +0530

    fixed an error or an extra backslash

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ passport.use(new LocalStrategy((username, password, done) => { //done is a callb
   passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'https://mmdp.onrender.com//auth/google/mmd'
+    callbackURL: 'https://mmdp.onrender.com/auth/google/mmd'
   }, async function(accessToken, refreshToken, profile, done) {
     try {
       // console.log(profile);
